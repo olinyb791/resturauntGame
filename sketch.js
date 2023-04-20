9fbe23ce5e86159a4989b73b2eebf344ce9da68a
Oliver <olinyb791@edu.linkoping.se>
2023-04-20 08:33:50 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+class Player{
+  constructor(x,y,r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+
+  move(){
+    
+  }
+
+}
+
+function setup() {
+  createCanvas(600,600);
+}
+
+function draw() {
+  background(220);
+}
